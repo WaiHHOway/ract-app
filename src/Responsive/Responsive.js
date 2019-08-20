ad0e7877de9709c3aa78164f51d446caf07b7efc
waihynhtun <waihynhtun@owaytrip.com>
2019-08-20 11:35:24 +0630

    first commit of react

---
+++ src/Responsive/Responsive.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
+
+const Example = () => {
+ 
+  return (
+    <div>
+         <h1>This is responsive</h1>
+    </div>
+  )
+}
+
+export default Example;
